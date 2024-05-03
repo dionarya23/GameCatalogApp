be786f5d263cb5aa85b3edcbe331b8cc5c135ab2
Dion Arya Pamungkas <dionarya.p@gmail.com>
2024-05-03 21:47:03 +0700

    Initial Commit

---
+++ Game Catalog Dicoding Submission/Game_Catalog_Dicoding_SubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_Catalog_Dicoding_SubmissionApp.swift
+//  Game Catalog Dicoding Submission
+//
+//  Created by Dion Arya Pamungkas on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Game_Catalog_Dicoding_SubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
